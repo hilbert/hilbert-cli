0e2cad658d482d9a1f86f14a4b91a7a7daf994cc
Oleksandr Motsak <http://goo.gl/mcpzY>
2016-11-15 04:05:31 +0100

    Added another unittest for checking warnings due to shaddowed mapping keys

---
+++ config/tests/test_loadwarnings.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+# encoding: utf-8
+# coding: utf-8
+
+from __future__ import absolute_import, print_function, unicode_literals  # NOQA
+
+from ..hilbert_cli_config import load_yaml
+
+import pytest                        # NOQA
+
+def load(s):
+    return load_yaml(s)
+
+class TestLoad:
+    def test_1(self, capsys):
+        out, err = capsys.readouterr()        
+        load('{a, b, a}')       
+        out, err = capsys.readouterr()
+        
+#        with capsys.disabled():
+        assert err == ''
+        assert out == """\
+WARNING: Key re-definition within some mapping: 
+K[line: 1, column: 2]: Previous Value: 
+ a: None
+ ↑
+---
+K[line: 1, column: 8]: New Value: 
+       a: None
+       ↑
+---
+===
+"""
+
+
+
+    def test_2(self, capsys):
+        out, err = capsys.readouterr()        
+        load("""{ ? a, ? b, ? a }""")
+        out, err = capsys.readouterr()
+        
+        assert err == ''
+        assert out == """\
+WARNING: Key re-definition within some mapping: 
+K[line: 1, column: 5]: Previous Value: 
+    a: None
+    ↑
+---
+K[line: 1, column: 15]: New Value: 
+              a: None
+              ↑
+---
+===
+"""
+   
+        
+
+
+        
+        
