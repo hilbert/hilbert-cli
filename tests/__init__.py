d5934efeec3c8659cf02950da3ce9de4aece6db2
Oleksandr Motsak <http://goo.gl/mcpzY>
2016-11-26 18:38:00 +0100

    Added Requirements + removed some unnecessary imports

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,13 @@
-from __future__ import absolute_import
-from __future__ import unicode_literals
+from __future__ import absolute_import, unicode_literals
 
-import sys
-
-if sys.version_info >= (2, 7):
-    import unittest  # NOQA
-else:
-    import unittest2 as unittest  # NOQA
-
-try:
-    from unittest import mock
-except ImportError:
-    import mock  # NOQA
+#import sys
+#
+#if sys.version_info >= (2, 7):
+#    import unittest  # NOQA
+#else:
+#    import unittest2 as unittest  # NOQA
+#
+#try:
+#    from unittest import mock 
+#except ImportError:
+#    import mock  # NOQA
